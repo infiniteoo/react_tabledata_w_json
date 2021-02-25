7cf773f1d6b686b7c57122f5463b6af3ccc796fd
Troy Dorman <troydorman@gmail.com>
2021-02-25 16:54:25 -0700

    added a fixed length to the dropdown box and started touchbing up the readme.md

--- src/components/DropDownFilter/DropDownFilter.js
+++ src/components/DropDownFilter/DropDownFilter.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
+import './DropDownFilter.css'
 
 
 
