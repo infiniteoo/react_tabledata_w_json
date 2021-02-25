2ce0d4d30ea58690111be218bf48af21a05a9c15
Troy Dorman <troydorman@gmail.com>
2021-02-25 09:53:24 -0700

    built getHeader function, binded it, and added its functionality to the JSX render

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -5,10 +5,18 @@ class Table extends Component {
   constructor(props) {
     super(props)
     this.getKeys = this.getKeys.bind(this)
+    this.getHeader = this.getHeader.bind(this)
   }
 
   getKeys = function() {
       return Object.keys(this.props.data[0])
+  }
+
+  getHeader = function() {
+      const keys = this.getKeys()
+      return keys.map((key) => {
+          return <th key={key}></th>
+      })
   }
 
   render() {
@@ -16,7 +24,7 @@ class Table extends Component {
       <div>
         <table>
           <thead>
-            <tr></tr>
+            <tr>{this.getHeader()}</tr>
           </thead>
           <tbody></tbody>
         </table>
