7ee3c70388e8eaba6044dccd94ce383ee40ab737
Troy Dorman <troydorman@gmail.com>
2021-02-25 09:49:36 -0700

    set up the getkeys function within the table component.  also binded the method to the class constructor

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,10 +1,16 @@
-import React, { Component } from "react";
-import "Table.css";
+import React, { Component } from "react"
+import "Table.css"
 
 class Table extends Component {
   constructor(props) {
-    super(props);
+    super(props)
+    this.getKeys = this.getKeys.bind(this)
   }
+
+  getKeys = function() {
+      return Object.keys(this.props.data[0])
+  }
+
   render() {
     return (
       <div>
