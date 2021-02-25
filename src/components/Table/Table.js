fef9a9550b640c30ce94b74f7919212b54b1319b
Troy Dorman <troydorman@gmail.com>
2021-02-25 09:47:09 -0700

    created Table class component and the basic table layout in JSX

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,2 +1,22 @@
-import React from 'react'
-import 'Table.css'
+import React, { Component } from "react";
+import "Table.css";
+
+class Table extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <table>
+          <thead>
+            <tr></tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Table;
