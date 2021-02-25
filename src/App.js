0f15d0042ac5a4e75c8500c8f6212bcac9768230
Troy Dorman <troydorman@gmail.com>
2021-02-25 09:44:04 -0700

    created Table component and refactored App from function to class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+import tableData from './user_data.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = tableData
+  }
+
+  render() {
+    return(
+
+
+      
+        <h1>hi friends</h1>
+      
+
+
+    )
+  }
+
+
 }
 
 export default App;
