d742d8685d228987998203b83defecff8013751c
Troy Dorman <troydorman@gmail.com>
2021-02-28 10:53:03 -0700

    after removing duplicates in the array by converting to a Set everything seems to work perfect now!  MVP is complete for this project.

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,28 +1,21 @@
-import React, { useState, useEffect } from 'react'
-import './SearchForm.css'
+import "./SearchForm.css";
 
 function SearchForm(props) {
-   
-
-    
-    
-    return (
-        <form className='search'>
-            <div className='form-group'>
-                
-                <input
-                value={props.search}
-                onChange={props.handleInputChange}
-                name='term'
-                type='text'
-                className='form-control'
-                placeholder='type in a search term to begin'
-                id='term'
-            />
-
-            </div>
-        </form>
-    )
+  return (
+    <form className="search">
+      <div className="form-group">
+        <input
+          value={props.search}
+          onChange={props.handleInputChange}
+          name="term"
+          type="text"
+          className="form-control"
+          placeholder="filter table data with supplied search terms"
+          id="term"
+        />
+      </div>
+    </form>
+  );
 }
 
-export default SearchForm
+export default SearchForm;
--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -3,7 +3,6 @@ import "./Table.css";
 import DropDownFilter from "../DropDownFilter/DropDownFilter.js";
 import SearchForm from "../SearchForm/SearchForm.js";
 
-
 class Table extends Component {
   constructor(props) {
     super(props);
@@ -27,7 +26,7 @@ class Table extends Component {
     console.log(data);
     this.setState({ data });
   }
-  
+
   getKeys = function () {
     return Object.keys(this.props.data[0]);
   };
@@ -55,42 +54,36 @@ class Table extends Component {
       );
     });
   };
-  handleInputChange = event => {
-   
-   var results = []
-   var toSearch = event.target.value
-   for(var i=0; i<this.props.data.length; i++) {
-    for(const key in this.props.data[i]) {
-      if(this.props.data[i][key].indexOf(toSearch)!==-1) {
-        results.push(this.props.data[i]);
+  handleInputChange = (event) => {
+    var results = [];
+    var toSearch = event.target.value;
+    for (var i = 0; i < this.props.data.length; i++) {
+      for (const key in this.props.data[i]) {
+        if (this.props.data[i][key].indexOf(toSearch) !== -1) {
+          results.push(this.props.data[i]);
+        }
       }
     }
-  }
 
-   console.log('value of filteredArray after filter: ', results)
-   this.setState({
-     data: results
-   })
-   this.getRows();
+    let uniqueArray = [...new Set(results)]
+
+    this.setState({
+      data: uniqueArray,
+    });
+    /* this.getRows(); */
   };
-
 
   render() {
     return (
       <div>
         <h1 id="h1-title">Sortable Data Table w/React.js</h1>
-        <div className='row'>
-          <div className='col-md-2'>
-          <DropDownFilter />
+        <div className="row">
+          <div className="col-md-2">
+            <DropDownFilter />
           </div>
-          <div className='col-md-10'>
-          <SearchForm
-          handleInputChange={this.handleInputChange}
-          /* results={search} */
-        
-          />
+          <div className="col-md-10">
+            <SearchForm handleInputChange={this.handleInputChange} />
           </div>
-          
         </div>
 
         <hr />
